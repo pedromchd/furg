26d52226cabd7aa3a84d0d934300f695f96605fc
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2025-05-05 18:25:03 +0000

    Add cost function formula of the selection sort algorithm

--- AED2/sort.c
+++ AED2/sort.c
@@ -11,19 +11,19 @@ ORDENA(A)
         swap(A[i], A[minIndex])
 
 SELECTION-SORT(A)
-    n <- length[A];                     C1
-    for j <- 1 to n - 1 do              C2 * n
-        smallest <- j;                  C3 * (n - 1)
-        for i <- j + 1 to n do          C4 * (n - 1) * (n - 1 + 1)
-            if A[i] < A[smallest] then  C5 * (n - 1) * n
-                smallest <- i;          C6 * (n - 1) * n
-            enf if;
-        enf for;
-        tmp <- A[j];                    C7 * (n - 1)
-        A[j] <- A[smallest];            C8 * (n - 1)
-        A[smallest] = tmp;              C9 * (n - 1)
-    end for;
-    end procedure
+    n <- length[A];                     C_1 = 1
+    for j <- 1 to n - 1 do              C_2 = ((n - 1) - 1) + 1 + 1 = n
+        smallest <- j;                  C_3 = n - 1
+        for i <- j + 1 to n do          C_4 = (n - (j + 1)) + 1 + 1 = n - j + 1  j = 1;      n - 1 + 1 = n
+            if A[i] < A[smallest] then  C_5 = n - j = (n^2 - n)/2                j = 2;      n - 2 + 1 = n - 1
+                smallest <- i;          C_6 = T6 = 0 (BC) / (n^2 - n)/2 (WC)     j = 3;      n - 3 + 1 = n - 2
+            enf if;                     C_7 = (n^2 - n)/2                       ...
+        enf for;                        C_8 = n - 1                              j = n - 1   n - (n - 1) + 1 = 2
+        tmp <- A[j];                    C_9 = n - 1                              somatorio(j = 1, n - 1, n - j + 1)
+        A[j] <- A[smallest];            C10 = n - 1                              => (n + 2)(n - 1)/2 = (n^2 + n - 2)/2
+        A[smallest] = tmp;              C11 = n - 1
+    end for;                            C12 = 1
+    end procedure                       C13 = 1
 */
 
 #include "stdio.h"
